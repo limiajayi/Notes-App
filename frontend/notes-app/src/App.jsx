06a90abdf489fc7092644f18b43f58dbbd71ee12
Temi <limiajayi@gmail.com>
2025-08-24 18:49:09 +0100

    Added tailwind to index, react-router-dom and component routes to app.

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Home from "./pages/Home/Home";
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path="/dashboard" exact element={<Home />} />
+    </Routes>
+  </Router>
+);
 
 const App = () => {
     return (
       <div>
-        <Home />
+        im app
+        {routes}
       </div>
     )
 }
