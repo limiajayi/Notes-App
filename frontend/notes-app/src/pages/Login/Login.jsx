6410fbc2bdb38e44274f5469a689b84a11110826
Temi <limiajayi@gmail.com>
2025-08-24 22:06:36 +0100

    Login form now done.

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,23 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import { Link } from "react-router-dom";
 import PasswordInput from "../../components/Input/PasswordInput";
+import { validateEmail } from "../../utils/helper";
 
 const Login = () => {
-    return (<>
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState(null)
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        //if the email is not valid
+        if (!validateEmail(email)) {
+            setError("Please enter a valid email address.")
+            return
+        }
+        if (!password) {
+            setError("Please enter the password.")
+            return
+        }
+
+        setError("")
+        
+        //Login API Call
+    }
+
+    return (
+    <>
     <Navbar />
 
     <div className="flex items-center justify-center mt-2">
 
         <div className="w-96 border rounded bg-white px-7 py-10">
-            <form onSubmit={() => {}}>
+            <form onSubmit={handleLogin}>
                 <h4 className="text-2xl mb-7">
                     Login
                 </h4>
 
-                <input type="text" placeholder="Email" className="input-box"/>
+                <input 
+                type="text" 
+                placeholder="Email" 
+                className="input-box"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                />
 
-                <PasswordInput placeholder={"Password"} />
+                <PasswordInput 
+                value={password}
+                onChange={(e) =>  setPassword(e.target.value)}
+                placeholder={"Password"} 
+                />
+
+                {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+
                 <button type="submit" className="btn-primary">
                     Login
                 </button>
@@ -25,7 +62,7 @@ const Login = () => {
                 <p className="text-sm text-center mt-4">
                     Not registered yet?{" "}
 
-                    <Link to="/signup" className="font-medium text-primary underline">
+                    <Link to="/signup" className="font-medium text-blue-300 underline">
                         Create an Account
                     </Link>
                 </p>
