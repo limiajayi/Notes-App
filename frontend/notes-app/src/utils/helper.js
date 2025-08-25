15d6a9d52cdb12a5a15174ba95e409842a8a2984
Temi <limiajayi@gmail.com>
2025-08-25 11:36:00 +0100

    Nav bar profile done.

--- frontend/notes-app/src/utils/helper.js
+++ frontend/notes-app/src/utils/helper.js
@@ -2,3 +2,18 @@ export const validateEmail = (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
 };
+
+export const getInitials = (name) => {
+    if (!name) return "";
+
+    //first name and last name array
+    const words = name.split(" ");
+    let initials = "";
+
+    for (let word in words) {
+        initials += words[word][0];
+    }
+
+    return initials.toUpperCase();
+
+}
