90a0274374b8bb2b2955e070b21d5423a9b3d950
Temi <limiajayi@gmail.com>
2025-08-25 13:05:49 +0100

    Individual notes done.

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import NoteCard from "../../components/Cards/NoteCard";
 
 const Home = () => {
     return (
         <>
             <Navbar />
 
-            
+            <div className="container mx-auto">
+                <div className="grid grid-cols-3 gap-4 mt-8">
+                    <NoteCard 
+                        title="Meeting Notes"
+                        date="3rd Apr 2024"
+                        content="Meeting on 7th April Meeting on 7th AprilMeeting on 7th April"
+                        tags="#Meeting"
+                        isPinned={true}
+                        onEdit={() => {}}
+                        onDelete={() => {}}
+                        onPinNote={() => {}}
+                    />
+
+                </div>
+            </div>
         </>
     )
 }
