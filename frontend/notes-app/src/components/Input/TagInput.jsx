f3a3224ab1594922191eb858aba1d19fa92696ae
Temi <limiajayi@gmail.com>
2025-08-26 11:50:10 +0100

    Done with the add notes and tag input components.

--- frontend/notes-app/src/components/Input/TagInput.jsx
+++ frontend/notes-app/src/components/Input/TagInput.jsx
@@ -1,18 +1,79 @@
+import { useState } from "react"
 import { MdAdd, MdClose } from "react-icons/md"
 
 
 const TagInput = ({ tags, setTags }) => {
+
+    //variable for the value of the tags
+    const [inputValue, setInputValue] = useState("");
+
+    //function for the onChange attribute of input tag
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value)
+        console.log(inputValue)
+    }
+
+    //function for the button tag below
+    const addNewTag = () => {
+        //if inputValue with the white space removed is not empty
+        if (inputValue.trim() !== "") {
+            
+            //set the value of tags to be a copy of the previous tags + the new inputValue
+            setTags([...tags, inputValue.trim()])
+        }
+    }
+
+    const handleKeyDown = (e) => {
+
+        //if the event object's key is enter, call addNewTag
+        if (e.key === "Enter") {
+            addNewTag()
+        }
+    }
+
+    //for the tags in props
+    const handleRemoveTag = (tagToRemove) => {
+        setTags(tags.filter(tag => tag !== tagToRemove))
+    }
+
     return (
         <div>
+
+            {
+                tags?.length > 0 && (
+                    <div className="flex items-center gap-2 flex-wrap mt-2">
+                        {
+                            tags.map((tag, index) => {
+                                return (
+                                    <span key={index} className="flex items-center gap-2 text-sm text-slate-900 bg-slate-100 px-3 py-1 rounded">
+                                        # {tag}
+                                        <button 
+                                        onClick={() => handleRemoveTag(tag)}>
+                                            <MdClose />
+                                        </button>
+                                    </span>
+                                )
+                            })
+                        }
+                    </div>
+                )
+            }
+
             <div className="flex items-center gap-4 mt-3">
+                {/* for input to be a controlled element it must have a value attribute and an onChange attribute! */}
                 <input 
                 type="text"
-                className="w-[50%] text-sm bg-transparent border-slate-700 px-3 py-2 rounded outline-none"
-                placeholder="Add Tags" 
+                className="w-[50%] text-sm bg-transparent border-slate-300 border-2 px-3 py-2 rounded outline-none"
+                placeholder="Add Tags"
+                value={inputValue}
+                onChange={handleInputChange}
+                onKeyDown={handleKeyDown}
                 />
 
-                <button className="w-8 h-8 flex items-center justify-center rounded drop-shadow-2xl border-blue-700 hover:bg-blue-700">
-                    <MdAdd className="text-2xl text-blue-700 hover:text-white" />
+                <button 
+                    className="w-8 h-8 flex items-center justify-center rounded border-blue-700 border-2 hover:bg-blue-700"
+                    onClick={addNewTag}>
+                        <MdAdd className="text-2xl text-blue-700 hover:text-white" />
                 </button>
             </div>
         </div>
--- frontend/notes-app/src/pages/Home/AddEditNotes.jsx
+++ frontend/notes-app/src/pages/Home/AddEditNotes.jsx
@@ -1,18 +1,71 @@
+import { useState } from "react"
 import TagInput from "../../components/Input/TagInput"
+import { MdClose } from "react-icons/md"
 
 
-const AddEditNotes = () => {
+const AddEditNotes = ({ noteData, type, onClose }) => {
+
+    const [title, setTitle] = useState("")
+    const [content, setContent] = useState("")
+    const [tags, setTags] = useState([])
+
+    const [error, setError] = useState(null)
+
+    // Add new note
+    const addNewNote = async () => {
+
+    }
+
+    // edit note
+    const editNote = async () => {
+        
+    }
+
+    const handleAddNotes = () => {
+        //if title is empty, show an error message
+        if (!title) {
+            setError("Please enter the title.")
+            return
+        }
+
+        //if content is empty, show an error message
+        if (!content) {
+            setError("Please enter the content.")
+            return
+        }
+
+        setError("")
+
+        if (type === "edit") {
+            editNote()
+        } else {
+            addNewNote()
+        }
+    }
 
     return (
-        <div>
+        <div className="relative">
+
+            <button
+                className="w-10 h-10 rounded-full flex items-center justify-center absolute -top-3 -right-3 hover:bg-slate-100"
+                onClick={onClose}>
+                    <MdClose className="text-xl text-slate-400" />
+            </button>
+
             <div className="flex flex-col gap-2">
                     <label className="input-label">TITLE</label>
+
+                    {/* for input to be a controlled element
+                    we must add a value and onchange attribute
+                    here the function body destructures the event object */}
+
                     <input 
                         type="text" 
                         className="text-2xl text-slate-950 outline-none"
                         placeholder="Go To Gym At 5"
+                        value={title}
+                        onChange={({ target }) => {setTitle(target.value)}}
                     />
-
             </div>
 
             <div className="flex flex-col gap-2 mt-4">
@@ -22,16 +75,22 @@ const AddEditNotes = () => {
                         className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
                         placeholder="Content"
                         rows={10}
+                        value={content}
+                        onChange={({ target }) => {setContent(target.value)}}
                     />
-
             </div>
 
             <div className="mt-3">
                     <label className="input-label">TAGS</label>
-                    <TagInput />
+                    <TagInput 
+                    tags={tags}
+                    setTags={setTags}
+                    />
             </div>
 
-            <button className="btn-primary font-medium mt-5 p-3" onClick={() => {}}>
+            {error && <p className="text-red-500 text-xs pt-4">{error}</p>}
+
+            <button className="btn-primary font-medium mt-5 p-3" onClick={handleAddNotes}>
                 ADD
             </button>
         </div>
